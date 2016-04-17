2c3d1cc568f9359e94c4bc807c8a4529d244f114
Samuel Bilý <samobily1@gmail.com>
2016-04-17 22:16:34 +0200

    Filter

--- app/src/main/java/com/bily/samuel/kviz/MainActivity.java
+++ app/src/main/java/com/bily/samuel/kviz/MainActivity.java
@@ -1,19 +1,25 @@
 package com.bily.samuel.kviz;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.database.CursorIndexOutOfBoundsException;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -23,6 +29,7 @@ import com.bily.samuel.kviz.lib.adapter.TestArrayAdapter;
 import com.bily.samuel.kviz.lib.database.DatabaseHelper;
 import com.bily.samuel.kviz.lib.database.Test;
 import com.bily.samuel.kviz.lib.database.User;
+import com.dd.processbutton.iml.ActionProcessButton;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -31,19 +38,22 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener{
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     private TextView nameText;
     private TextView emailText;
     private SwipeRefreshLayout swipeRefreshLayout;
 
     private TestArrayAdapter testArray;
-
+    private FragmentManager fm;
+    private ActionProcessButton button;
     private DatabaseHelper db;
     private GetTests getTests;
     private int id;
     private String regId;
     private String PROJECT_NUMBER = "821868220510";
+    private String test ="";
+    private String instructor = "";
     private User user;
 
     @Override
@@ -53,14 +63,14 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         setContentView(R.layout.activity_main);
         db = new DatabaseHelper(getApplicationContext());
         user = db.getUser();
-        if(user == null){
+        if (user == null) {
             Intent i = new Intent(getApplicationContext(), LoginActivity.class);
             startActivity(i);
             finish();
-        }else{
-            try{
+        } else {
+            try {
                 loadDataToListView();
-            }catch(IllegalStateException e){
+            } catch (IllegalStateException e) {
                 Intent i = new Intent(getApplicationContext(), LoginActivity.class);
                 finish();
                 startActivity(i);
@@ -71,19 +81,38 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
             emailText.setText(user.getEmail());
             id = user.getId_u();
 
-            swipeRefreshLayout = (SwipeRefreshLayout)findViewById(R.id.swipaMain);
+            swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipaMain);
             swipeRefreshLayout.setOnRefreshListener(this);
             swipeRefreshLayout.setColorSchemeColors(R.color.colorLigth, R.color.colorDark);
             swipeRefreshLayout.setRefreshing(true);
 
-            testArray = new TestArrayAdapter(getApplicationContext(),R.layout.list_test);
+            testArray = new TestArrayAdapter(getApplicationContext(), R.layout.list_test);
             getTests = new GetTests();
             getTests.execute();
         }
     }
 
     @Override
-    protected void onResume(){
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.find:
+                filter();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onResume() {
         super.onResume();
         getTests = new GetTests();
         getTests.execute();
@@ -96,16 +125,16 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         id = user.getId_u();
     }
 
-    public void onProfileButtonClicked(View view){
+    public void onProfileButtonClicked(View view) {
         Intent i = new Intent(getApplicationContext(), ProfileActivity.class);
         startActivity(i);
     }
 
-    public void loadDataToListView(){
-        try{
+    public void loadDataToListView() {
+        try {
             final ArrayList<Test> tests = db.getTests();
-            testArray = new TestArrayAdapter(getApplicationContext(),R.layout.list_test);
-            for(int i = 0; i<tests.size();i++){
+            testArray = new TestArrayAdapter(getApplicationContext(), R.layout.list_test);
+            for (int i = 0; i < tests.size(); i++) {
                 Test test = tests.get(i);
                 testArray.add(test);
             }
@@ -126,9 +155,15 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
                     startActivity(i);
                 }
             });
-        }catch(CursorIndexOutOfBoundsException e){
+        } catch (CursorIndexOutOfBoundsException e) {
             e.printStackTrace();
         }
+    }
+
+    public void filter() {
+        fm = getSupportFragmentManager();
+        SetFilter setFilter = new SetFilter();
+        setFilter.show(fm, "Filter");
     }
 
     @Override
@@ -137,34 +172,63 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         getTests.execute();
     }
 
-    private class GetTests extends AsyncTask<Void,Void,Void>{
+    @SuppressLint("ValidFragment")
+    public class SetFilter extends DialogFragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            final View rootView = inflater.inflate(R.layout.fragment_filter, container, false);
+            getDialog().setTitle("Filter");
+            final EditText editTest = (EditText)rootView.findViewById(R.id.editTestName);
+            final EditText editInstructor = (EditText)rootView.findViewById(R.id.editInstructor);
+            button = (ActionProcessButton) rootView.findViewById(R.id.btnLogIn);
+            button.setMode(ActionProcessButton.Mode.ENDLESS);
+            button.setColorScheme(getResources().getColor(R.color.colorLigth), getResources().getColor(R.color.colorAccent), getResources().getColor(R.color.colorPrimary), getResources().getColor(R.color.colorDark));
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    button.setProgress(50);
+                    test = editTest.getText().toString();
+                    instructor = editInstructor.getText().toString();
+                    getTests = new GetTests();
+                    getTests.execute();
+                    button.setProgress(0);
+                }
+            });
+            return rootView;
+        }
+    }
+
+
+    private class GetTests extends AsyncTask<Void, Void, Void> {
 
         @Override
         protected Void doInBackground(Void... params) {
             JSONParser jsonParser = new JSONParser();
-            HashMap<String,String> values = new HashMap<>();
-            values.put("tag","getTests");
-            values.put("id_u","" + user.getId_u());
+            HashMap<String, String> values = new HashMap<>();
+            values.put("tag", "getTests");
+            values.put("id_u", "" + user.getId_u());
+            values.put("test", "%"+test+"%");
+            values.put("instructor", "%"+instructor+"%");
             try {
                 JSONObject jsonObject = jsonParser.makePostCall(values);
                 Log.e("gettin", jsonObject.toString());
-                if(jsonObject.getInt("success")==1){
+                if (jsonObject.getInt("success") == 1) {
                     JSONArray tests = jsonObject.getJSONArray("tests");
                     db.dropTests();
-                    for(int i = 0; i < tests.length();i++){
+                    for (int i = 0; i < tests.length(); i++) {
                         JSONObject testJ = tests.getJSONObject(i);
                         Test test = new Test();
                         test.setIdT(testJ.getInt("id"));
                         test.setName(testJ.getString("name"));
                         test.setAnswered(testJ.getInt("answered"));
                         test.setInstructor(testJ.getString("instructor"));
-                        if(testJ.getInt("answered")>0){
-                            test.setQuestions("" + testJ.getDouble("stat")+ "%");
+                        if (testJ.getInt("answered") > 0) {
+                            test.setQuestions("" + testJ.getDouble("stat") + "%");
                         }
                         db.storeTest(test);
                     }
 
-                }else{
+                } else {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
@@ -174,7 +238,7 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
                     });
                 }
             } catch (JSONException e) {
-                Log.e("JSON",e.toString());
+                Log.e("JSON", e.toString());
             }
 
             runOnUiThread(new Runnable() {
